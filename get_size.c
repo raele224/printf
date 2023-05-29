01d1d29ff67593c5e615237092ac948ca0a5e974
OlgaMuravha <olgamuravha@gmail.com>
2023-05-29 07:31:41 -0700

    printf

--- get_size.c
+++ get_size.c
@@ -1,10 +1,8 @@
 #include "main.h"
-#define S_LONG 1
-#define S_SHORT 2
 /**
- * get_size - Calculates the size to cast the argument.
- * @format: Formatted string that will print the arguments.
- * @i: List of arguments that will be printed.
+ * get_size - Calculates the size to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
  * Return: Precision.
  */
 int get_size(const char *format, int *i)
